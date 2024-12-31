7d281ed0363f899bd3d5862d56233cc8ab93bff4
AbdouGG <robloxg503@gmail.com>
2024-12-31 11:23:54 +0100

    fix56

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,8 @@
 // Linkvertise URLs for each checkpoint
 export const LINKVERTISE_URLS = {
   1: 'https://link-target.net/1174023/check-point-1',
-  2: 'https://link-target.net/1174023/check-point-2',
-  3: 'https://link-target.net/1174023/check-point-3',
+  2: 'https://link-target.net/1174023/check-point-2-nurk',
+  3: 'https://link-target.net/1174023/check-point-3-nurk',
 };
 
 // Target URLs for Linkvertise (what to put in the target URL field)
